84eccdae742ae0f86ffcd5c6c9d3c2c76de905b0
Aditya <adityam874@gmail.com>
2024-03-06 01:03:39 +0530

    5 march learning complete

---
+++ 09-mvc-rest-api/controllers/employeesController.js
@@ -0,0 +1,38 @@
+const data = {
+    employees: require('../model/employees.json'),
+    setEmployees: function (data) { this.employees = data }
+};
+
+const getAllEmployees = (req, res) => {
+    res.json(data.employees);
+}
+
+const createNewEmployees = (req, res) => {
+    res.json({
+        'firstname': req.body.firstname,
+        'lastname': req.body.lastname
+    })
+}
+
+const updateEmployee = (req, res) => {
+    res.json({
+        'firstname': req.body.firstname,
+        'lastname': req.body.lastname
+    });
+}
+
+const deleteEmployee = (req, res) => {
+    res.json({ "id": req.body.id });
+}
+
+const getEmployee = (req, res) => {
+    res.json({ 'id': req.param.id });
+}
+
+module.exports = {
+    getAllEmployees,
+    createNewEmployees,
+    updateEmployee,
+    deleteEmployee,
+    getEmployee
+}
